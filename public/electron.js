17ceca046e34d72882bad75f82b383917124ce8f
Tristan-coder <dev.tristan.romero@gmail.com>
2019-10-29 12:25:29 -0600

    minimal template

---
+++ public/electron.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    //** You can configure properties what you need  */
+    width: 800,
+    height: 600
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  mainWindow.on("close", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
